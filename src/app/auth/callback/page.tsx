7c3f82a1064f8cc81041ff9d221f062c380fe06a
Giusepe V.R <giusepe.vr@gmail.com>
2025-08-23 20:42:01 -0600

    Fix spotify oauth flow v2

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import spotifyAuth from '@/utils/spotify';
 
 export default function AuthCallback() {
   const router = useRouter();
-  const searchParams = useSearchParams();
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
